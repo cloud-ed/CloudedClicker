5f21fc33e75eab79b5ecd98b9f7b5cb61ff36dee
Josh <f360f2@outlook.com>
2025-04-10 13:31:05 +1000

    feat: :sparkles: basic clicking loop implemented

--- autoclicker.py
+++ autoclicker.py
@@ -3,6 +3,20 @@ import keyboard
 import time
 
 clicking = False
-delay = 0.1
+delay = 0.05
 
-print("Press 's' to start/stop clicking. Press 'q' to quit.")
+print("Press '`' to start/stop clicking. Press 'q' to quit.")
+
+while True:
+    if keyboard.is_pressed('`'):
+        clicking = not clicking
+        print('Started clicking...' if clicking else 'Stopped clicking.')
+        time.sleep(0.5) # Debounce the button to avoid accidentally pressing multiple times
+    
+    if keyboard.is_pressed('q'):
+        print('Exiting program.')
+        break
+
+    if clicking:
+        pyautogui.click()
+        time.sleep(delay)
